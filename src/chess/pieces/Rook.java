53180244b5f9e45f79519d2147d2c6483c79b892
RedSlifer <cassio_tavaresrh@hotmail.com>
2021-12-09 22:20:13 -0400

    Implement possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,54 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
 
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] auxMatrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+        //move up
+        auxPosition.setValues(position.getRow() - 1, position.getColumn());
+
+        while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            auxPosition.setRow(auxPosition.getRow() - 1);
+        }
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //move left
+        auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+
+        while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            auxPosition.setColumn(auxPosition.getColumn() - 1);
+        }
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //move right
+        auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+
+        while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            auxPosition.setColumn(auxPosition.getColumn() + 1);
+        }
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //move down
+        auxPosition.setValues(position.getRow() + 1, position.getColumn());
+
+        while (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            auxPosition.setRow(auxPosition.getRow() + 1);
+        }
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        return auxMatrix;
     }
 }
