6e2476669a4c82b56240a1e9a7049302e2d2213d
RedSlifer <cassio_tavaresrh@hotmail.com>
2022-01-07 22:29:21 -0400

    Implementing Castling logic

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,12 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-    public King(Board board, Color color) {
+
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -19,6 +24,12 @@ public class King extends ChessPiece {
         ChessPiece auxPiece = (ChessPiece) getBoard().piece(position);
 
         return (auxPiece == null) || (auxPiece.getColor() != getColor());
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece auxPiece = (ChessPiece) getBoard().piece(position);
+
+        return (auxPiece instanceof Rook) && (auxPiece.getColor() == getColor()) && (auxPiece.getMoveCount() == 0);
     }
 
     @Override
@@ -80,6 +91,34 @@ public class King extends ChessPiece {
 
         if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
             auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // castling
+        if (getMoveCount() == 0 && !chessMatch.isCheck()) {
+            // kingside
+            Position positionR1 = new Position(position.getRow(), position.getColumn() + 3);
+
+            if (testRookCastling(positionR1)) {
+                Position auxPosition1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position auxPosition2 = new Position(position.getRow(), position.getColumn() + 2);
+
+                if (getBoard().piece(auxPosition1) == null && getBoard().piece(auxPosition2) == null) {
+                    auxMatrix[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+
+            // queenside
+            Position positionR2 = new Position(position.getRow(), position.getColumn() - 4);
+
+            if (testRookCastling(positionR2)) {
+                Position auxPosition1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position auxPosition2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position auxPosition3 = new Position(position.getRow(), position.getColumn() - 3);
+
+                if (getBoard().piece(auxPosition1) == null && getBoard().piece(auxPosition2) == null && getBoard().piece(auxPosition3) == null) {
+                    auxMatrix[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
         }
 
         return auxMatrix;
